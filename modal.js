5f988e1c93beb0140e935b116e837e2a2afff17e
kloom <pavelfesenko.work@gmail.com>
2021-01-18 19:58:56 +0200

    change adaptiv and index.css and index.html

--- modal.js
+++ modal.js
@@ -1,11 +1,56 @@
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
+//
+//     const modalButtons = document.querySelectorAll('.open-modal'),
+//         overlay = document.querySelector('.overlay-modal'),
+//         closeButtons = document.querySelectorAll('.modal-close'),
+//         modalElem = document.querySelector('#modal')
+//
+//     modalButtons.forEach(function(item){
+//         item.addEventListener('click', function(e) {
+//             e.preventDefault();
+//             modalElem.classList.add('active');
+//             overlay.classList.add('active');
+//         });
+//     });
+//
+//     closeButtons.forEach(function(item){
+//
+//         item.addEventListener('click', function(e) {
+//             const parentModal = this.closest('#modal');
+//             parentModal.classList.remove('active');
+//             overlay.classList.remove('active');
+//         });
+//     });
+//
+//     document.body.addEventListener('keyup', function (e) {
+//         const key = e.key;
+//
+//         if (key === 'Escape') {
+//             document.querySelector('.modal.active').classList.remove('active');
+//             document.querySelector('.overlay').classList.remove('active');
+//         };
+//     }, false);
+//
+//     overlay.addEventListener('click', function() {
+//         document.querySelector('.modal.active').classList.remove('active');
+//         this.classList.remove('active');
+//     });
+//
+// });
+//
+//
 
-    const modalButtons = document.querySelectorAll('.open-modal'),
-        overlay      = document.querySelector('.overlay-modal'),
-        closeButtons = document.querySelectorAll('.modal-close'),
-        modalElem = document.querySelector('#modal')
 
-    modalButtons.forEach(function(item){
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const modalButtons = document.querySelectorAll('.open-modal');
+    const overlay = document.querySelector('.overlay-modal');
+    const closeButtons = document.querySelectorAll('.modal-close');
+    const modalElem = document.querySelector('#modal');
+
+    modalButtons.forEach((item) => {
         item.addEventListener('click', function(e) {
             e.preventDefault();
             modalElem.classList.add('active');
@@ -13,8 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    closeButtons.forEach(function(item){
-
+    closeButtons.forEach((item) =>{
         item.addEventListener('click', function(e) {
             const parentModal = this.closest('#modal');
             parentModal.classList.remove('active');
@@ -37,3 +81,5 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
+
--- validate.js
+++ validate.js
@@ -1,3 +1,63 @@
-$(document).ready(function() {
-    $("#phonenumber").mask("+38 (099) 99-99-999");
-});
+// $(document).ready(function() {
+//     $("#phonenumber").mask("+38 (099) 99-99-999");
+// });
+
+let firstName = document.querySelector('.form-input-name');
+let firstName1 = document.querySelector('.form-input-name1');
+let firstEmail = document.querySelector('.form-input-email');
+
+firstName.oninput = function (){
+    let error_my = document.querySelector('.error');
+    // let valid = document.querySelector('.my-valid');
+    console.log(firstName.value);
+    if(firstName.value.length < 3){
+        error_my.style.visibility = "visible";
+        firstName.classList.add('invalid-input');
+    }else {
+        firstName.classList.add('my-valid');
+        firstName.classList.remove('invalid-input');
+        error_my.style.visibility = "hidden";
+    }
+    check();
+}
+
+firstName1.oninput = () =>{
+    let error_my = document.querySelector('.error1');
+    // let valid = document.querySelector('.my-valid');
+    console.log(firstName1.value);
+    if(firstName1.value.length < 3){
+        error_my.style.visibility = "visible";
+        firstName1.classList.add('invalid-input');
+    }else {
+        firstName1.classList.add('my-valid');
+        firstName1.classList.remove('invalid-input');
+        error_my.style.visibility = "hidden";
+    }
+    check();
+}
+firstEmail.oninput = () =>{
+    let error_my = document.querySelector('.error2');
+    // let valid = document.querySelector('.my-valid');
+    console.log(firstEmail.value);
+    if(firstEmail.value.length < 3){
+        error_my.style.visibility = "visible";
+        firstEmail.classList.add('invalid-input');
+    }else {
+        firstEmail.classList.add('my-valid1');
+        firstEmail.classList.remove('invalid-input');
+        error_my.style.visibility = "hidden";
+    }
+    check();
+}
+
+function check(){
+    let checkInputName = document.querySelector('.form-input-name');
+    let checkInputName1 = document.querySelector('.form-input-name1');
+    let checkInputEmail = document.querySelector('.form-input-email');
+    let formBtn = document.querySelector('.form-account');
+    if (checkInputName.value.length < 3 && checkInputName1.value.length < 3 && checkInputEmail.value.length < 0 ){
+        formBtn.setAttribute('disabled', 'disabled');
+    }else{
+        formBtn.removeAttribute('disabled');
+    }
+}
